39006f35d44080cbfffa9a476eb7af884bf13b39
Alejandro <alejandronicolasdelalamo@gmail.com>
2025-01-26 11:55:10 +0000

    First commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+
+// import Home from "./views/Home.jsx";
+import Contacts from "./views/Contacts.jsx";
+import AddContact from "./views/AddContact.jsx";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import EditContact from "./views/EditContact.jsx";
 
 //create your first component
 const Layout = () => {
@@ -19,19 +17,17 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
+				<Routes>
+					{/* <Route path="/" element={<Home />} /> */}
+					<Route path="/" element={<Contacts />} />
+					<Route path="/addContact" element={<AddContact />} />
+					<Route path="/editContact/:id" element={<AddContact />} />
+					<Route path="*" element={<h1>Not found!</h1>} />
+
+				</Routes>
 			</BrowserRouter>
 		</div>
 	);
 };
 
-export default injectContext(Layout);
+export default injectContext(Layout)
